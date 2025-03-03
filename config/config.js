c383def712844d0df7dac671e1bb8e193d751d72
FATIH FIKRY OKTAVIANTO <viananto1234@gmail.com>
2025-03-03 15:06:45 +0700

    Ubah

--- config/config.js
+++ config/config.js
@@ -1,23 +1,17 @@
-const { Sequelize } = require('sequelize');
-const mysql2 = require('mysql2');
 require('dotenv').config();
 
-// Debug: Cek apakah env terbaca dengan benar
-console.log("🔍 DB Config:");
-console.log("Host:", process.env.DB_HOST);
-console.log("Port:", process.env.DB_PORT);
-console.log("User:", process.env.DB_USER);
-console.log("Database:", process.env.DB_NAME);
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT || 'mysql',
-  dialectModule: mysql2, // Tambahkan ini untuk kompatibilitas dengan Vercel
-  dialectOptions: {
-    connectTimeout: 60000,
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    dialectModule: require('mysql2'),
+    dialectOptions: {
+      connectTimeout: 60000,
+    },
+    logging: false,
   },
-  logging: false,
-});
-
-module.exports = sequelize;
+};
