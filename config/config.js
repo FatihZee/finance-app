50cceb3cb84a47887ffae24583bbfdd5a342ebf5
FATIH FIKRY OKTAVIANTO <viananto1234@gmail.com>
2025-03-03 15:32:22 +0700

    `Added .env and .vercel to .gitignore, updated config/config.js with test and production database configurations, and updated models/index.js to use the new database configurations.`

--- config/config.js
+++ config/config.js
@@ -14,4 +14,27 @@ module.exports = {
     },
     logging: false,
   },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    dialectModule: require('mysql2'),
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: process.env.DB_DIALECT || 'mysql',
+    dialectModule: require('mysql2'),
+    dialectOptions: {
+      connectTimeout: 60000,
+    },
+    logging: false,
+  },
 };
